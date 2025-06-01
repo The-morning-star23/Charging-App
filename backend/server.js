c486fda33f2ed63f356d62753c6f8c9005362b9e
The-morning-star23 <kumarshubh263@gmail.com>
2025-06-01 12:44:33 +0530

    made chnages in backend\package.json and added vercel.json in backend

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,11 @@
 const app = require("./app");
 const PORT = process.env.PORT || 5000;
 
+app.get('/', (req, res) => {
+  res.send('Backend running on Vercel');
+});
+
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
